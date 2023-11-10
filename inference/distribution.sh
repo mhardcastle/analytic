4384064d1e4af2e5389a8b7954c682033acc6001
Martin Hardcastle <mjh@extragalactic.info>
2023-11-10 17:22:57 +0000

    update path

--- inference/distribution.sh
+++ inference/distribution.sh
@@ -1,3 +1,3 @@
 cd /beegfs/car/mjh/inference-z1.25
 export PYTHONPATH=/home/mjh/git/analytic:$PYTHONPATH
-python /home/mjh/git/analytic/paper/run_distribution_qsub.py $1 >>run-${1}-out.txt 2>&1
+python /home/mjh/git/analytic/inference/run_distribution_qsub.py $1 >>run-${1}-out.txt 2>&1
