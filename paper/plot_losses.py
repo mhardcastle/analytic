2b264772d50bf308c2095d27df0628c7cbca65f4
Martin Hardcastle <mjh@extragalactic.info>
2025-12-26 12:26:22 +0000

    New style import

--- paper/plot_losses.py
+++ paper/plot_losses.py
@@ -1,7 +1,7 @@
 from solver import Evolve_RG
 import numpy as np
 import matplotlib.pyplot as plt
-from constants import *
+from synch_constants import *
 from matplotlib import rc
 
 rc('font',**{'family':'serif','serif':['Times'],'size':14})
